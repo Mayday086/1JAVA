71e9d5245b36c3db9ea67915b5c4ca2dfc946b47
raman04-byte <ramantank04022002@gmail.com>
2022-10-18 16:12:26 +0530

    Update Password.java

--- Password.java
+++ Password.java
@@ -1,10 +1,13 @@
 import java.io.Console;
 class Password{
+	// MAIN METHOD
 	public static void main(String args[]){
+		// USING CONSOLE CLASS 
 		Console con=System.console();
 		System.out.print("Enter Password:	");
+		// READING PASSWORD FROM THE CONSOLE
 		String pass=new String(con.readPassword());
-		
+		// PRINTING THE PASSWORD FROM THE CONSOLE
 		System.out.println("Password is " + pass);
 	}
 }
